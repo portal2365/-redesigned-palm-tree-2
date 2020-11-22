d0345f19360e410fb229bec42611db571db85956
portal2365 <43701661+portal2365@users.noreply.github.com>
2020-11-22 15:06:43 +0000

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+
+<script src="https://www.gstatic.com/firebasejs/8.1.1/firebase-app.js"></script>
+
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDsw336q_DwpDdxv_6do-FkJ0FrGjLxO5E",
+    authDomain: "bed-time-stories-1f69c.firebaseapp.com",
+    databaseURL: "https://bed-time-stories-1f69c.firebaseio.com",
+    projectId: "bed-time-stories-1f69c",
+    storageBucket: "bed-time-stories-1f69c.appspot.com",
+    messagingSenderId: "597584950336",
+    appId: "1:597584950336:web:86d869c7234a44fc5e1ba5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
